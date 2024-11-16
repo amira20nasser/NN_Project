5696f4f4b06e46ebd1247fccd364c72cc97e82e4
mohammed24266ahmed <154350993+mohammed24266ahmed@users.noreply.github.com>
2024-11-16 13:20:23 +0200

    Update Evaluator.py

--- Evaluator.py
+++ Evaluator.py
@@ -1,21 +1,20 @@
-# any evaluation function like confusion_matrix
 import numpy as np
-from sklearn.metrics import accuracy_score
-
 
 class Evaluator:
     @staticmethod
     def compute_confusion_matrix(y_actual, y_pred):
-        tp = np.sum((y_actual == 1) & (y_pred == 1))
-        tn = np.sum((y_actual == -1) & (y_pred == -1))
-        fp = np.sum((y_actual == -1) & (y_pred == 1))
-        fn = np.sum((y_actual == 1) & (y_pred == -1))
+        
+        tp = np.sum((y_actual == 1) & (y_pred == 1))  # True Positives
+        tn = np.sum((y_actual == -1) & (y_pred == -1))  # True Negatives
+        fp = np.sum((y_actual == -1) & (y_pred == 1))  # False Positives
+        fn = np.sum((y_actual == 1) & (y_pred == -1))  # False Negatives
+
+     
         cm = np.array([[tn, fp], [fn, tp]])
         return cm
 
     @staticmethod
     def overall_accuracy(y_actual, y_pred):
-        return accuracy_score(y_actual, y_pred)
-
-
-
+        tn, fp, fn, tp = Evaluator.compute_confusion_matrix(y_actual, y_pred).ravel()
+        accuracy = (tp + tn) / (tp + tn + fp + fn)
+        return accuracy

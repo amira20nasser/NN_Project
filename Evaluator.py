b4b0aa1c8fe57f3a8a9e6e417deb4f1ba3cf9790
Amna <ammy5342@gmail.com>
2024-11-16 13:53:39 +0200

    (fix) Fixed manual confusion matrix

--- Evaluator.py
+++ Evaluator.py
@@ -1,20 +1,27 @@
+# any evaluation function like confusion_matrix
 import numpy as np
+from sklearn.metrics import accuracy_score
+
 
 class Evaluator:
     @staticmethod
+    @staticmethod
     def compute_confusion_matrix(y_actual, y_pred):
-        
-        tp = np.sum((y_actual == 1) & (y_pred == 1))  # True Positives
-        tn = np.sum((y_actual == -1) & (y_pred == -1))  # True Negatives
-        fp = np.sum((y_actual == -1) & (y_pred == 1))  # False Positives
-        fn = np.sum((y_actual == 1) & (y_pred == -1))  # False Negatives
-
-     
-        cm = np.array([[tn, fp], [fn, tp]])
-        return cm
+        TP = FP = TN = FN = 0
+        for actual, pred in zip(y_actual, y_pred):
+            if actual == 1 and pred == 1:
+                TP += 1
+            elif actual == -1 and pred == 1:
+                FP += 1
+            elif actual == -1 and pred == -1:
+                TN += 1
+            elif actual == 1 and pred == -1:
+                FN += 1
+        return np.array([[TN, FN], [FP, TP]])
 
     @staticmethod
     def overall_accuracy(y_actual, y_pred):
-        tn, fp, fn, tp = Evaluator.compute_confusion_matrix(y_actual, y_pred).ravel()
-        accuracy = (tp + tn) / (tp + tn + fp + fn)
-        return accuracy
+        correct = sum(1 for actual, pred in zip(y_actual, y_pred) if actual == pred)
+        total = len(y_actual)
+        return correct / total
+
